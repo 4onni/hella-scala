f28a2a75738c89fae3c622f87b13061b7c7fda5b
zhangqinghua <zhangqinghua@zhangqinghua-EOS.(none)>
2014-05-28 18:00:20 +0800

    first commit! idea+scala plugin+sbt plugin

---
+++ build.sbt
@@ -0,0 +1,4 @@
+name := "hella-scala"
+
+version := "1.0"
+    
---
+++ src/main/scala/HelloScala.scala
@@ -0,0 +1,10 @@
+/**
+ * Created by zhangqinghua on 14-5-28.
+ */
+object HelloScala {
+
+  def main(args: Array[String]) {
+    println("hello scala!")
+  }
+
+}
